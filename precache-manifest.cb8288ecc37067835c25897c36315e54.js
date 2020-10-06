508b20a0cfbc8a8fbdee4ccd2c2e8a56af28a85c
Ralph Effting <ralfting@gmail.com>
2020-10-06 08:13:19 -0300

    Updates

--- precache-manifest.cb8288ecc37067835c25897c36315e54.js
+++ precache-manifest.cb8288ecc37067835c25897c36315e54.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5bacdbf12b0d2ea8ae926bcc8d1f7849",
+    "revision": "fdf134837d24f6507141d806192ac2ed",
     "url": "/gh-pages/index.html"
   },
   {
-    "revision": "86a558f6144f3d7676da",
+    "revision": "a56b32e01ac74ee5d164",
     "url": "/gh-pages/static/css/main.5f361e03.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/gh-pages/static/js/2.a094c3e5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "86a558f6144f3d7676da",
-    "url": "/gh-pages/static/js/main.ed03f6aa.chunk.js"
+    "revision": "a56b32e01ac74ee5d164",
+    "url": "/gh-pages/static/js/main.80b2525c.chunk.js"
   },
   {
     "revision": "40105ffac423ab7aa803",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gh-pages/precache-manifest.ac40cf2a08167700b27e5f0bbda957ca.js"
+  "/gh-pages/precache-manifest.cb8288ecc37067835c25897c36315e54.js"
 );
 
 self.addEventListener('message', (event) => {
