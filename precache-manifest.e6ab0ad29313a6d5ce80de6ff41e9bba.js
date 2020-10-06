2ca64c9d8d798650b65ecf33fd80a326c38d85ef
Ralph Effting <ralfting@gmail.com>
2020-10-06 07:44:26 -0300

    Updates

---
+++ precache-manifest.e6ab0ad29313a6d5ce80de6ff41e9bba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c51827c84a87b93d1e7f453a9523fbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "553cdaece053c5a10e32",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "049837a534165a1e8646",
+    "url": "/static/js/2.3add2df1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3add2df1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "553cdaece053c5a10e32",
+    "url": "/static/js/main.f1b7bc1b.chunk.js"
+  },
+  {
+    "revision": "1c6b8093d38ef6d6bbe1",
+    "url": "/static/js/runtime-main.16f25903.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
